20ca162ebccfe8e3761b634c14c4c81918d17a20
ZeRcooI <mihanpan1996@mail.ru>
2023-12-29 03:23:54 +0300

    ReserveCopy

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private Vector2Int _gridSize;
+
+    private Dictionary<Vector2Int, Node> _grid = new Dictionary<Vector2Int, Node>();
+
+    private void Awake()
+    {
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        for (int x = 0; x < _gridSize.x; x++)
+        {
+            for (int y = 0; y < _gridSize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x, y);
+
+                _grid.Add(coordinates, new Node(coordinates, true));
+            }
+        }
+    }
+}
